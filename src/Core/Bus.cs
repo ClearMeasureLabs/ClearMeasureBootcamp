36b9326a9a30c6bdf3c8facb518804b0003cff8e
Tony <tonyjen0905@gmail.com>
2017-08-26 21:37:17 -0400

    Added comments for Bus class.

--- src/Core/Bus.cs
+++ src/Core/Bus.cs
@@ -7,7 +7,7 @@ namespace ClearMeasure.Bootcamp.Core
 
     public class Bus
     {
-        private readonly SingleInstanceFactory _singleInstanceFactory;
+        private readonly SingleInstanceFactory _singleInstanceFactory; // create single instance factory
 
         public Bus()
         {
@@ -17,7 +17,10 @@ namespace ClearMeasure.Bootcamp.Core
         {
             _singleInstanceFactory = singleInstanceFactory;
         }
-
+        /**
+         * Send request wrapped in IRequest interface. TResponse is the return value for this method as well as 
+         * IRequest interface.
+        */
         public virtual TResponse Send<TResponse>(IRequest<TResponse> request)
         {
             var defaultHandler = GetHandler(request);
